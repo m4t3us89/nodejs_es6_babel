4d29ec2a2727f518a23389713bff4b2efa8fa58d
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-08 11:03:30 -0300

    Adicionado o Morgan

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import api from './services/axios'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 
 app.get( '' , async (req,res) => {
