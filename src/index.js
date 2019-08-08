fa0296e1bbea0fbebe930b87649f937b910d7976
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-08 11:17:09 -0300

    Ordernacao

--- src/index.js
+++ src/index.js
@@ -7,16 +7,24 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 
+const orderBy = function(data,order = 'asc'){
+    return data.sort( (a,b)=> {
+        if( order == 'asc' ) {
+            if(  a.title  >  b.title  ) return 1
+            if(  a.title  <  b.title  ) return -1
+        }else{
+            if(  a.title  <  b.title  ) return 1
+            if(  a.title  >  b.title  ) return -1
+        }
+        return 0
+    } )
+}
+
 app.get( '' , async (req,res) => {
     try{
         const { data } = await api.get('/todos')
-        const todosOrderByAsc = data.sort( (a,b)=> {
-            if( a.title > b.title  ) return 1
-            if( a.title < b.title  ) return -1
-            return 0
-        } )
-
-        return res.status(201).send( todosOrderByAsc )
+        const todosOrderBy = orderBy(data,req.query.order)
+        return res.status(201).send( todosOrderBy )
     }catch(err){
         return res.status(400).send( {
             message: 'Error'
